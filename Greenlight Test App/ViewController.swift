12c4d10da459f4b6750ec7909f899d6c8a84bf39
Yehor Shapanov <iegor.sharapov@lyv.livenation.com>
2018-11-14 08:44:30 -0800

    Initial commit

---
+++ Greenlight Test App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Greenlight Test App
+//
+//  Created by Yehor Shapanov on 11/14/18.
+//  Copyright © 2018 Yehor Shapanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
